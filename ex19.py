5c0be978ec52ba4bba68cac5f60496b1d3fb2845
Johnny Martinez <johnny@Johnnys-MacBook-Pro-2.local>
2019-10-23 06:54:44 -0600

    modified for python3

--- ex19.py
+++ ex19.py
@@ -8,7 +8,8 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
 print("We can just give the function numbers directly:")
 cheese_and_crackers(20, 30)
 
-print("Or, we can use variables from our script:")
+
+print("Orsss, we can use variables from our script:")
 amount_of_cheese = 10
 amount_of_crackers = 50
 
--- ex21.py
+++ ex21.py
@@ -1,23 +1,35 @@
 def add(a, b):
-    print "Adding %d + %d" % (a, b)
+    print(f"Adding {a} + {b}")
     return a + b
 
-def subtract(a, b):
-    print "SUBTRACTING %d - %d" % (a, b)
 
-def multiply (a, b):
-    print "Multiplying %d * %d" % (a, b)
-    return a * b 
+def subtract(a, b):
+    print(f"SUBTRACTING {a} - {b}")
+    return a - b
+
+
+def multiply(a, b):
+    print(f"Multiplying {a} * {b}")
+    return a * b
+
 
 def divide(a, b):
-    print "DIVIDING %d / %d" % (a,b)
+    print(f"DIVIDING {a} / {b}")
     return a / b
 
-print "Let's do some math with just functions."
+
+print("Let's do some math with just functions.")
 
 age = add(30, 5)
 height = subtract(78, 4)
 weight = multiply(90, 2)
 iq = divide(100, 2)
 
-print "Age:" %d "Height:" %d "Weight:" %d "IQ:" %d" % (age, height, weight, id)
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
+
+# A puzzle for the extra credit, type it in anyway.
+print("Here is a puzzle.")
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print("That becomes: ", what, "Can you do it by hand?")
