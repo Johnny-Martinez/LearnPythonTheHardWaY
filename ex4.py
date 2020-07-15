71ce7b2d45eb3bb24e2132f7bc26c8075cd70b9c
Johnny Martinez <johnny.j.martinez@outlook.com>
2020-07-14 18:00:24 -0600

    running through some exercises

--- ex4.py
+++ ex4.py
@@ -1,22 +1,16 @@
 cars = 100
-# floating point variable for space in cars
 space_in_car = 4.0
-# number of drivers available
 drivers = 30
-# number of passengers available
-passangers = 90
-# remaining number of cars after all seats have been taken
+passengers = 90
 cars_not_driven = cars - drivers
-# amount of cars driven is equal to drivers on the road
 cars_driven = drivers
-# number of drivers times amount of space in vehicle.
-carpool_capacity = drivers * space_in_car
-# average number of seats taken per vehicle on the road.
-average_passenger_per_car = passangers / cars_driven
+carpool_capacity = cars_driven * space_in_car
+average_passengers_per_car = passengers / cars_driven
 
-print('There are ', cars, ' cars available.')
-print('There are only ', drivers, ' drivers available')
-print('There will be ', cars_not_driven, ' empty cars today.')
-print('We can transport ', carpool_capacity, ' people today.')
-print('We have ', passangers, ' to carpool today.')
-print('We need to put about ', average_passenger_per_car, ' in each car.')
+
+print("There are ", cars, "cars available.")
+print("There are only", drivers, "drivers available.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today")
+print("We have", passengers, "to carpool today.")
+print("We need to put about", average_passengers_per_car, "in each car")
--- ex5.py
+++ ex5.py
@@ -1,7 +1,7 @@
 # Variable for my name
 name = 'Johnny M'
 # Variable for my age
-age = 37
+age = 38
 # Variable for my height
 height = 68
 # Variable for my weight
@@ -13,18 +13,13 @@ teeth = 'White'
 # variable for color of hair
 hair = 'Brown'
 
-centimeters = height * 2.54
-kilograms = weight * 2.205
-
-print(f"Lets's talk about {name}.")
+print(f"Let's talk about {name}.")
 print(f"He's {height} inches tall.")
-print(f"and {centimeters} centimeters tall.")
-print(f"He's {weight} pounds heavy.")
-print(f"Which converts to {kilograms} Kilograms")
-print(f"Actually, thats not too heavy.")
+print(f"He's {weight} pounds heavy")
+print(f"Actually... thats not too heavy.")
 print(f"He's got {eyes} eyes and {hair} hair.")
 print(f"His teeth are usually {teeth} depending on coffee.")
 
-# this line is tricky, try to get it exactly right.
+
 total = age + height + weight
-print(f"If I add {age}, {height}, and {weight} I get a {total}")
+print(f"If I add {age}, {height}, and {weight} I get {total}")
