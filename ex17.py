f7d412da23c24d59702700b638129fcb695fbff5
Johnny Martinez <johnny.j.martinez@outlook.com>
2021-01-07 01:19:40 -0700

    updating exercizes

--- ex17.py
+++ ex17.py
@@ -5,14 +5,13 @@ script, from_file, to_file = argv
 
 print(f"Copying from {from_file} to {to_file}")
 
-# We could do these two on one line, how?
-
 in_file = open(from_file)
 indata = in_file.read()
 
 print(f"The input file is {len(indata)} bytes long")
 
 print(f"Does the output file exist? {exists(to_file)}")
+
 print("Ready, hit RETURN to continue, CTRL-C to abort.")
 input()
 
--- ex18.py
+++ ex18.py
@@ -1,26 +1,18 @@
-# This one is like your scripts with argv
 def print_two(*args):
-    arg1, arg2 = args
+    arg1, arg2, = args
     print(f"arg1: {arg1}, arg2: {arg2}")
-
-# Ok, that *args is actually pointless... We can just do the following
-
-
+    
 def print_two_again(arg1, arg2):
-    print(f"arg1: {arg1}, arg2: {arg2}")
-
-# This just takes one arguement
-
-
+    print(f"arg1: {arg1} arg2: {arg2}")
+    
+#this just takes one argument
 def print_one(arg1):
     print(f"arg1: {arg1}")
-
-# this takes no arguements
-
-
+    
+#This one takes no arguements
 def print_none():
-    print_one("I got nothin'.")
-
+    print("I aint got nuthin.")
+    
 
 print_two("Zed", "Shaw")
 print_two_again("Zed", "Shaw")
